0641ff66a7e0aa1144b58519843628077d890eed
Pavel547 <pawukok11@gmail.com>
2024-12-09 20:53:05 +0100

    За сьогодні я дописав операції шляху для письменика та трохи підкорегував круд операції

--- app/crud.py
+++ app/crud.py
@@ -9,20 +9,24 @@ def create_writer(db: Session, writer: schemas.WriterCreate):
     db.refresh(create_writer_in_db)
     return create_writer_in_db
 
-def get_writer_by_name(db: Session, writer_name: str):
-    return db.query(models.Writer).filter(models.Writer.name==writer_name).first()
+def get_writer_by_name(db: Session, name: str):
+    return db.query(models.Writer).filter(models.Writer.name==name).first()
 
-def get_writer_by_id(db: Session, writer_id: int):
-    return db.query(models.Writer).filter(models.Writer.id==writer_id).first()
+def get_writer_by_id(db: Session, id: int):
+    return db.query(models.Writer).filter(models.Writer.id==id).first()
 
-def get_writer_by_pseudonym(db: Session, writer_pseudo: str):
-    return db.query(models.Writer).filter(models.Writer.pseudonym==writer_pseudo).first()
+def get_writer_by_pseudonym(db: Session, pseudo: str):
+    return db.query(models.Writer).filter(models.Writer.pseudonym==pseudo).first()
 
 def get_writers(db: Session, skip: int = 0, limit: int = 20):
     return db.query(models.Writer).offset(skip).limit(limit).all()
 
 def get_items(db: Session, skip: int = 0, limit: int = 20):
     return db.query(models.Book).offset(skip).limit(limit).all()
+
+def del_writer(db: Session, writer: schemas.Writer):
+    db.delete(writer)
+    db.commit()
 
 def create_book(db: Session, book: schemas.BookCreate, author_name: str):
     create_book_in_db = models.Book(
@@ -43,8 +47,20 @@ def get_book_by_id(db: Session, id: int):
 def get_book_by_name(db: Session, name: str):
     return db.query(models.Book).filter(models.Book.name==name).first()
 
-def get_book_by_author(db: Session, author_name: str):
-    return db.query(models.Book).filter(models.Book.author==author_name).first()
+def get_book_by_author(db: Session, name: str):
+    return db.query(models.Book).filter(models.Book.author==name).first()
 
 def get_books(db: Session, skip: int = 0, limit: int = 20):
     return db.query(models.Book).offset(skip).limit(limit).all()
+
+def update_book(db: Session, book_name: str, update_date: schemas.BookBase):
+    book = db.query(models.Book).filter(models.Book.name==book_name).first()
+    for key, value in update_date.model_dump().items():
+        setattr(book, key, value)
+    db.commit()
+    db.refresh(book)
+    return book
+
+def del_book(db: Session, book: schemas.Book):
+    db.delete(book)
+    db.commit()
--- app/main.py
+++ app/main.py
@@ -3,3 +3,48 @@ from sqlalchemy.orm import Session
 
 from app import crud, models, schemas
 from app.database import SessionLocall, engine
+
+models.Base.metadata.create_all(bind=engine)
+
+mini_lib = FastAPI()
+
+def get_db():
+    db = SessionLocall()
+    try:
+       yield db
+    finally:
+        db.close
+        
+@mini_lib.post("/create_writer/", response_model=schemas.WriterCreate)
+def add_writer(writer: schemas.WriterCreate, db: Session = Depends(get_db)):
+    create_writer = crud.get_writer_by_name(db=db, writer_name=writer.name)
+    if create_writer is None:
+        raise HTTPException(status_code=400, detail="Writer already exists")
+    return crud.create_writer(db=db, writer=writer)
+
+@mini_lib.get("/get_writer/name/{writer_name}", response_model=schemas.Writer)
+def get_writer_by_name(name: str, db: Session = Depends(get_db)):
+    get_writer = crud.get_writer_by_name(db=db, writer_name=name)
+    if get_writer is None:
+        raise HTTPException(status_code=400, detaili="Writer not found")
+    return get_writer
+
+@mini_lib.get("/get_writer/id/{writer_id}", response_model=schemas.Writer)
+def get_writer_by_id(writer_id: int, db: Session = Depends(get_db)):
+    get_writer = crud.get_writer_by_id(db=db, id=writer_id)
+    if get_writer is None:
+        raise HTTPException(status_code=404, detail="Writer not found")
+    return get_writer
+
+@mini_lib.get("/get_writer/pseudonym/{writer_pseudonym}", response_model=schemas.Writer)
+def get_writer_by_pseudonym(writer_pseudonym: str, db: Session = Depends(get_db)):
+    get_writer = crud.get_writer_by_pseudonym(db=db, pseudo=writer_pseudonym)
+    if get_writer is None:
+        raise HTTPException(status_code=404, detail="Writer not found")
+    return get_writer
+
+@mini_lib.get("/get_writers/", response_model=schemas.Writer)
+def get_writers(skip: int = 0, limit: int = 20, db: Session = Depends(get_db)):
+    get_writers = crud.get_writers(db=db, skip=skip, limit=limit)
+    return get_writers
+
